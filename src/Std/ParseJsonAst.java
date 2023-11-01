ad0e3173f3e9a2c502a8d2950856f9b80ad80b1f
Rarity Belle <rarity.vanlone@gmail.com>
2023-11-01 14:26:47 +0000

    fix: serializer serializes now

--- src/Std/ParseJsonAst.java
+++ src/Std/ParseJsonAst.java
@@ -3,12 +3,14 @@
 import java.util.*;
 import java.io.*;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 
 public class ParseJsonAst extends ProcessFiles {
 
     // Parse the program and call $ok() on the resulting parse tree
     public void action(Scan scn, Trace trace) {
         ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
         _Start parseTree = _Start.parse(scn, trace);
         parseTree.$ok();
         try {
