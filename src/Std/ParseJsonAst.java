d0bc7172f2bffc5876a073105eba2426cbb2c9f4
Rarity Belle <rarity.vanlone@gmail.com>
2023-11-01 19:37:05 +0000

    wip: show class names in generated JSON file

--- src/Std/ParseJsonAst.java
+++ src/Std/ParseJsonAst.java
@@ -1,9 +1,11 @@
-// WIP File
-
 import java.util.*;
 import java.io.*;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.jsontype.PolymorphicTypeValidator;
+import com.fasterxml.jackson.databind.jsontype.BasicPolymorphicTypeValidator;
+
+
 
 public class ParseJsonAst extends ProcessFiles {
 
@@ -13,6 +15,14 @@ public class ParseJsonAst extends ProcessFiles {
         objectMapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
         objectMapper.enable(SerializationFeature.WRAP_ROOT_VALUE);
         objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+
+        PolymorphicTypeValidator ptv = BasicPolymorphicTypeValidator
+        .builder()
+        .allowIfBaseType(Object.class) // Can change "Object" to any type, we can look back at this later to be more specific
+        .build();
+
+        objectMapper.activateDefaultTypingAsProperty(ptv, ObjectMapper.DefaultTyping.NON_FINAL, "$type");
+
         _Start parseTree = _Start.parse(scn, trace);
         parseTree.$ok();
         try {
