deed6c8fd2a771e8a785d33e87cc39e888ae3bd8
Stoney Jackson <dr.stoney@gmail.com>
2024-02-16 17:00:39 +0000

    refactor(gitpod): install bats

---
+++ bin/test/code.bash
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+PROJECT_ROOT="$( cd "${SCRIPT_DIR}/../.." &> /dev/null && pwd )"
+cd "${PROJECT_ROOT}"
+
+tests/run
