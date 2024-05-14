75728f97910a2471a741394ca90dceaecc6adedb
Stoney Jackson <dr.stoney@gmail.com>
2024-05-14 16:55:13 +0000

    parse lex rules

--- src/plcc/spec/__init__.py
+++ src/plcc/spec/__init__.py
@@ -1,17 +1,62 @@
 from __future__ import annotations
 from typing import Iterable
-
+from gettext import gettext as _
+import re
+from dataclasses import dataclass
 
 from ..specfile.line import Line
 
 
-def parse(lines: Iterable[Line]) -> Spec:
-    ...
+def parse(lines):
+    p = Parser()
+    p.parse(lines)
+    return p.getSpec()
 
 
-def Spec():
-    def parse(self, lines: Iterable[Line]):
-        ...
+class Parser():
+    def __init__(self):
+        self._spec = Spec()
 
-    def getSectionCount(self):
-        return 3
+    def parse(self, lines):
+        if lines is None:
+            raise TypeError(_('`lines` must be an iterable of `Line`.'))
+        self._parseLexicalSpecification(lines)
+
+    def _parseLexicalSpecification(self, lines):
+            for line in lines:
+                s = line.string.strip()
+                if not s:
+                    continue
+                if s[0] == '#':
+                    continue
+                m = re.match(r'^\s*(?:(?P<type>skip|token)\s+)?(?P<name>[A-Z_]+)\s+(?P<quote>[\'"])(?P<pattern>[^(?P=quote)]*)(?P=quote)(?P<end>.*)$', line.string)
+                d = m.groupdict()
+                if not d['type']:
+                    d['type'] = 'token'
+                d['line'] = line
+                self._spec.addLexRule(LexRule(**d))
+
+
+    def getSpec(self) -> Spec:
+        return self._spec
+
+
+class Spec():
+    def __init__(self):
+        self._lexRules = []
+
+    def addLexRule(self, rule):
+        self._lexRules.append(rule)
+
+    def getLexRules(self):
+        return self._lexRules
+
+
+@dataclass(frozen=True)
+class LexRule:
+    type: str
+    name: str
+    pattern: str
+    quote: str
+    end: str
+    line: Line
--- tests/unit/test_spec.py
+++ tests/unit/test_spec.py
@@ -2,12 +2,64 @@ import pytest
 
 
 from plcc.spec import parse
-from plcc.specfile import specfile
+from plcc.spec import LexRule
+from plcc.specfile.line import Line
 
 
-def test_parse_three_empty_sections(fs):
-    fs.create_file('/f', contents='''\
-%
-%
-''')
-    spec = parse(specfile('/f'))
+def test_parse_wrong_type(fs):
+    with pytest.raises(TypeError):
+        parse(None)
+
+
+def lines(string):
+    return iter([Line(string=s, path='', number=i) for i, s in enumerate(string.splitlines(), start=1)])
+
+
+def test_parse_skip_rule(fs):
+    spec = parse(lines(r"skip WS '\s+'"))
+    rules = spec.getLexRules()
+    assert rules[0] == LexRule(type='skip', name='WS', pattern=r'\s+', quote="'", end="", line=Line(string=r"skip WS '\s+'", path='', number=1))
+
+
+def test_parse_token_rule(fs):
+    spec = parse(lines(r"token WS '\s+'"))
+    rules = spec.getLexRules()
+    assert rules[0] == LexRule(type='token', name='WS', pattern=r'\s+', quote="'", end="", line=Line(string=r"token WS '\s+'", path='', number=1))
+
+
+def test_parse_implicit_token_rule(fs):
+    spec = parse(lines(r"WS '\s+'"))
+    rules = spec.getLexRules()
+    assert rules[0] == LexRule(type='token', name='WS', pattern=r'\s+', quote="'", end="", line=Line(string=r"WS '\s+'", path='', number=1))
+
+
+def test_allow_line_comment_in_lex_rule(fs):
+    spec = parse(lines(r"WS '\s+' # hi"))
+    rules = spec.getLexRules()
+    assert rules[0] == LexRule(type='token', name='WS', pattern=r'\s+', quote="'", end=" # hi", line=Line(string=r"WS '\s+' # hi", path='', number=1))
+
+
+def test_allow_pound_in_lex_rule_pattern(fs):
+    spec = parse(lines(r"COMMENT ' #\s+' # matching a comment"))
+    rules = spec.getLexRules()
+    assert rules[0] == LexRule(type='token', name='COMMENT', pattern=r' #\s+', quote="'", end=" # matching a comment", line=Line(string=r"COMMENT ' #\s+' # matching a comment", path='', number=1))
+
+
+def test_skip_blank_lines_in_lex(fs):
+    spec = parse(lines("\n\nCOMMENT ' #\\s+' # matching a comment"))
+    rules = spec.getLexRules()
+    assert rules[0] == LexRule(type='token', name='COMMENT', pattern=r' #\s+', quote="'", end=" # matching a comment", line=Line(string=r"COMMENT ' #\s+' # matching a comment", path='', number=3))
+
+
+def test_skip_comment_lines_in_lex(fs):
+    spec = parse(lines("#one\n  # two \nCOMMENT ' #\\s+' # matching a comment"))
+    rules = spec.getLexRules()
+    assert rules[0] == LexRule(type='token', name='COMMENT', pattern=r' #\s+', quote="'", end=" # matching a comment", line=Line(string=r"COMMENT ' #\s+' # matching a comment", path='', number=3))
+
+
+def test_allow_double_quote_in_lex(fs):
+    spec = parse(lines(r'WS "\s+"'))
+    rules = spec.getLexRules()
+    assert rules[0] == LexRule(type='token', name='WS', pattern=r'\s+', quote='"', end="", line=Line(string=r'WS "\s+"', path='', number=1))
+
+
