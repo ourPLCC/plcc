8c01cf4c0bb4a1dbcc900be9744b173d927f04e9
Michael Banerjee <michaelbanerjee10@gmail.com>
2024-11-13 02:11:28 +0000

    WIP cleaned code

--- src/plcc/load_spec/validate_spec/validate_syntactic_spec/build_parsing_table.py
+++ src/plcc/load_spec/validate_spec/validate_syntactic_spec/build_parsing_table.py
@@ -1,20 +1,25 @@
 from collections import defaultdict
-# an addition parameter of
-# impossible, because how would we know how to map cS to B
+
 class Table:
     def __init__(self):
         self._table = defaultdict(list)
 
-    def get(self, X, a):
+    def get(self, X: str, a: str) -> list[set[str]]:
         return self._table[(X,a)]
 
     def set(self, X, a, rhs):
         self._table[(X, a)] = {rhs}
 
-    def add(self, X, a, rhs):
+    def add(self, X:str, a:str, rhs:str):
         self._table[(X, a)].append({rhs})
 
-def build_parsing_table(first: dict[str, set[str]], follow: dict[str, set[str]], rules: dict[str, list[str]]): # -> Table: dict[(str, str): list[set[str]]]
+    def getKeys(self) -> list[tuple[str, str]]:
+        return list(self._table.keys())
+
+    def getValues(self):
+        return list(self._table.values())
+
+def build_parsing_table(first: dict[str, set[str]], follow: dict[str, set[str]], rules: dict[str, list[str]]) -> Table:
     return ParsingTableBuilder(first, follow, rules).build()
 
 class ParsingTableBuilder:
@@ -25,49 +30,42 @@ class ParsingTableBuilder:
         self.table = Table()
 
     def build(self):
-        self._addFirstSetRules()
+        self._addRulesToCorrectCells()
         return self.table
 
-    def _addFirstSetRules(self):
+    def _addRulesToCorrectCells(self):
         for nonterminal in self.rules.keys():
             for productionLists in self.rules[nonterminal]:
+
                 for production in productionLists:
                     if self._isNonterminalOnly(production) and self._isProductionInNonterminalsRule(production, nonterminal):
                         continue
-                    for t in self.first[production]:
-                        self.table.add(nonterminal, t, production)
+                    self.addFirstSetRulesIfValid(nonterminal, production)
 
                     if self._isEpsilonInSet(self.first[production]):
-                        for t in self.follow[nonterminal]:
-                            self.table.add(nonterminal, t, production)
+                        self._addFollowSetRulesIfValid(nonterminal, production)
 
-# epsilon is both a production and t
-
-    def _addFollowSetRules(self, nonterminal, production):
-        for t in self.follow[production]:
+    def addFirstSetRulesIfValid(self, nonterminal: str, production: str):
+        for t in self.first[production]:
+            if self._isTerminalEpisilon(t):
+                continue
             self.table.add(nonterminal, t, production)
 
+    def _addFollowSetRulesIfValid(self, nonterminal: str, production: str):
+        for t in self.follow[nonterminal]:
+            if self._isTerminalEpisilon(t):
+                continue
+            self.table.add(nonterminal, t, production)
+
+    def _isTerminalEpisilon(self, t: str):
+        return True if t == "" else False
 
     def _isNonterminalOnly(self, production):
         return True if production in self.rules.keys() else False
 
 
-    def _isProductionInNonterminalsRule(self, production, nonterminal):
+    def _isProductionInNonterminalsRule(self, production: str, nonterminal: str):
         return True if production in self.rules[nonterminal] else False
 
-    def _isEpsilonInSet(self, production):
-        return True if '' in production else False
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    def _isEpsilonInSet(self, s: set):
+        return True if '' in s else False
--- src/plcc/load_spec/validate_spec/validate_syntactic_spec/build_parsing_table_test.py
+++ src/plcc/load_spec/validate_spec/validate_syntactic_spec/build_parsing_table_test.py
@@ -1,11 +1,26 @@
 from pytest import raises
 from .build_parsing_table import build_parsing_table, Table
 from collections import defaultdict
+def test_empty():
+    firstSets = buildFirstSet([],[])
+    followSets = buildFollowSet([],[])
+    rules = buildRules([],[])
 
-def test_first_set_placement():
-    firstSets = buildBasicFirstSet()
-    followSets = buildBasicFollowSet()
-    rules = buildBasicRules()
+    table = build_parsing_table(firstSets, followSets, rules)
+    assert len(table.getKeys()) == 0
+
+def test_complex_not_LL1():
+    productions = ['d', 'b', 's', 'b C', 'd b', 'A B', 'C s', 'D', ""]
+    firstOfProductions = [{"D", ""}, {"A", "C"}, {"A", "C", "D"}, {"A", "C"}, {"D", "A", "C"}, {"A"}, {"C"}, {"D"}, {""}]
+    followOfProductions = [{"A", "C"}, {"C", chr(26)}, {chr(26), "C"}]
+
+    firstSets = buildFirstSet(productions, firstOfProductions)
+    followSets = buildFollowSet(productions, followOfProductions)
+
+    lhs = ["s", "b", "d"]
+    rhs = [["b C", "d b"],["A B", 'C s'],["D", ""]]
+    rules = buildRules(lhs, rhs)
+
     table = build_parsing_table(firstSets, followSets, rules)
     assert table.get('s', 'C') == [{"b C"}, {'d b'}]
     assert table.get('s', 'A') == [{"b C"}, {'d b'}]
@@ -15,34 +30,26 @@ def test_first_set_placement():
     assert table.get('d', 'A') == [{""}]
     assert table.get('d', 'C') == [{""}]
     assert table.get('d', 'D') == [{"D"}]
+    assert len(table.getKeys()) == 8
 
-def buildBasicFirstSet():
+
+def buildFirstSet(productions: list[str], firstOfProductions: list[set[str]]) -> dict[str, set[str]]:
     firstSets = defaultdict(set)
-    firstSets['d'] = {"D", ""}
-    firstSets['b'] = {"A", "C"}
-    firstSets['s'] = {"A", "C", "D"}
-    firstSets['b C'] = {"A", "C"}
-    firstSets['d b'] = {"D", "A", "C"}
-    firstSets['A B'] = {"A"}
-    firstSets['C s'] = {"C"}
-    firstSets['D'] = {"D"}
-    firstSets[""] = {""}
-
+    for i, production in enumerate(productions):
+        firstSets[production] = firstOfProductions[i]
     return firstSets
 
-def buildBasicFollowSet():
+def buildFollowSet(productions: list[str], followOfProductions: list[set[str]]) -> dict[str, set[str]]:
     followSets = defaultdict(set)
-    followSets["d"] = {"A", "C"}
-    followSets["b"] = {"C",chr(26)}
-    followSets["s"] = {chr(26), "C"} #chr(26) is EOL
-
+    for i, production in enumerate(productions):
+        try:
+            followSets[production] = followOfProductions[i]
+        except:
+            break
     return followSets
 
-def buildBasicRules():
+def buildRules(lhs: list[str], rhs: list[list[str]]) -> dict[str, list[list[str]]]:
     rules = defaultdict(list)
-    rules["s"] = [["b C", "d b"]]
-    rules["b"] = [["A B", 'C s']]
-    rules["d"] = [["D"],[""]]
-
+    for i, nonterminal in enumerate(lhs):
+        rules[nonterminal] = [rhs[i]]
     return rules
-
